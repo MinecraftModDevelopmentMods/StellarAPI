cf4277ca3a6b3c0baa6924cc8e8a474944b7b3ee
Abastro <abab9579@gmail.com>
2018-01-07 13:16:43 +0900

    Removed mundane events

--- src/main/java/stellarapi/SAPIForgeEventHook.java
+++ src/main/java/stellarapi/SAPIForgeEventHook.java
@@ -2,24 +2,18 @@ package stellarapi;
 
 import net.minecraft.entity.player.EntityPlayer.SleepResult;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
-import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import stellarapi.api.SAPICapabilities;
 import stellarapi.api.SAPIReferences;
-import stellarapi.api.event.world.ClientWorldEvent;
-import stellarapi.api.event.world.ServerWorldEvent;
 import stellarapi.api.helper.LivingItemAccessHelper;
 import stellarapi.api.interact.IOpticalProperties;
 import stellarapi.api.optics.IOpticalViewer;
 import stellarapi.reference.OpticalViewerEventCallback;
-import stellarapi.reference.PerServerManager;
 
 public class SAPIForgeEventHook {
 
@@ -79,33 +73,6 @@ public class SAPIForgeEventHook {
 				callback.updateFilter();
 
 			LivingItemAccessHelper.setUsingItem(event.getEntityLiving(), previous);
-		}
-	}
-
-	@SubscribeEvent(priority = EventPriority.HIGHEST)
-	public void onWorldLoad(WorldEvent.Load event) {
-		if (event.getWorld().isRemote)
-			SAPIReferences.getEventBus()
-					.post(new ClientWorldEvent.Load(event.getWorld()));
-		else {
-			MinecraftServer server = event.getWorld().getMinecraftServer();
-			// TODO Simplify this code - do PerServerManager really needed? Or not?
-			if (!PerServerManager.isInitiated(server)) {
-				SAPIReferences.getEventBus().post(new ServerWorldEvent.Initial(server, server.getEntityWorld()));
-				PerServerManager.initiatePerServerManager(server);
-			}
-
-			SAPIReferences.getEventBus().post(new ServerWorldEvent.Load(server, event.getWorld()));
-		}
-	}
-
-	@SubscribeEvent(priority = EventPriority.HIGHEST)
-	public void onWorldUnload(WorldEvent.Unload event) {
-		if (event.getWorld().isRemote)
-			SAPIReferences.getEventBus().post(new ClientWorldEvent.Unload(event.getWorld()));
-		else {
-			MinecraftServer server = event.getWorld().getMinecraftServer();
-			SAPIReferences.getEventBus().post(new ServerWorldEvent.Unload(server, event.getWorld()));
 		}
 	}
 
--- src/main/java/stellarapi/api/event/world/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * World load/unload subsection of the Event section of Stellar API.
- * */
-@API(apiVersion = "@STVERSION@", owner = SAPIReferences.MODID, provides = SAPIReferences.APIID)
-package stellarapi.api.event.world;
-
-
-import net.minecraftforge.fml.common.API;
-import stellarapi.api.SAPIReferences;
